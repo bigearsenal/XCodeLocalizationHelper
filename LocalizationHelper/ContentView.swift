80acfa6b798d6993722be0fbe639dce9b4bacbf8
Chung Tran <bigearsenal@gmail.com>
2020-10-17 10:39:34 +0700

    Initial Commit

---
+++ LocalizationHelper/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  LocalizationHelper
+//
+//  Created by Chung Tran on 10/17/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
