00f860657654761c1418f603ed780a0b6cba8779
Chung Tran <bigearsenal@gmail.com>
2020-10-17 11:22:59 +0700

    open xCode project

--- LocalizationHelper/ContentView.swift
+++ LocalizationHelper/ContentView.swift
@@ -8,9 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var currentProjectUrl: String? = UserDefaults.standard.string(forKey: "Settings.currentProjectUrl") {
+        didSet { UserDefaults.standard.set(currentProjectUrl, forKey: "Settings.currentProjectUrl") }
+    }
+
     var body: some View {
-        Text("Hello, World!")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        Group {
+            if let url = currentProjectUrl {
+                HStack {
+                    Text("Current project: " + url)
+                    openProject(title: "Other...")
+                }
+                
+                Spacer()
+                Text("Hello, World!")
+                Spacer()
+            } else {
+                openProject()
+            }
+        }
+        .frame(minWidth: 500, maxWidth: .infinity, minHeight: 500, maxHeight: .infinity)
+    }
+    
+    fileprivate func openProject(title: String = "Open XCode project") -> Button<Text> {
+        return Button(title) {
+            let dialog = NSOpenPanel();
+            
+            dialog.title                   = "Choose an xCode project's directory"
+            dialog.showsResizeIndicator    = true
+            dialog.showsHiddenFiles        = false
+            dialog.allowsMultipleSelection = false
+            dialog.canChooseDirectories    = true
+            dialog.canChooseFiles          = false
+            
+            if (dialog.runModal() ==  NSApplication.ModalResponse.OK) {
+                let result = dialog.url // Pathname of the file
+                
+                if let path = result?.path {
+                    self.currentProjectUrl = path
+                }
+                
+            } else {
+                // User clicked on "Cancel"
+                return
+            }
+        }
     }
 }
 
