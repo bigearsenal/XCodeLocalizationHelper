2ec47edbb9f0e06c6dc07eb0f3445919b9440b85
Chung Tran <bigearsenal@gmail.com>
2020-10-18 10:19:28 +0700

    add extension

---
+++ extension/SourceEditorCommand.swift
@@ -0,0 +1,19 @@
+//
+//  SourceEditorCommand.swift
+//  extension
+//
+//  Created by Chung Tran on 10/18/20.
+//
+
+import Foundation
+import XcodeKit
+
+class SourceEditorCommand: NSObject, XCSourceEditorCommand {
+    
+    func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) -> Void {
+        // Implement your command here, invoking the completion handler when done. Pass it nil on success, and an NSError on failure.
+        
+        completionHandler(nil)
+    }
+    
+}
