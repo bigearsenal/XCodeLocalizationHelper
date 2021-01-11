b4efcf15b6d8565d6c7d559c2f95483ad2af830e
Chung Tran <bigearsenal@gmail.com>
2021-01-11 16:48:20 +0700

    add first ruby script

--- ruby/index.rb
+++ ruby/index.rb
@@ -1,14 +1,29 @@
 require 'xcodeproj'
 
-# verify argument
-if ARGV.length != 1
-	puts "We need exactly 1 argument that is the path to your xcode project"
+# verify arguments
+if ARGV.length != 2
+	puts "We need exactly 2 arguments that is the path to your xcode project and location code"
 	exit
 end
 
-# open project
-# puts ARGV[0]
+# get arguments
 project_path = ARGV[0]
+location_code = ARGV[1]
+
+# open project
 project = Xcodeproj::Project.open(project_path)
 
+# add region
+known_regions = project.root_object.known_regions
+if !known_regions.include?(location_code)
+	known_regions.push(location_code)
+end
+
+# get target
 target = project.targets.first
+
+target.build_configurations.each do |config|
+	config.build_settings['CLANG_ANALYZER_LOCALIZABILITY_NONLOCALIZED'] = 'YES'
+end
+
+project.save
