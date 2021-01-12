af2a812217c1fdb22fea61d9f6a4431b6f6f2717
Chung Tran <bigearsenal@gmail.com>
2021-01-12 09:52:35 +0700

    add locale

--- ruby/index.rb
+++ ruby/index.rb
@@ -2,6 +2,22 @@ require 'xcodeproj'
 require 'pathname'
 require 'fileutils'
 
+# utilities
+def add_locale(project_dir, project_name, code)
+	lproj_folder = project_dir + "/" + project_name + "/" + code + ".lproj"
+	localizable_file = "Localizable.strings"
+	# create .lproj folder
+	if !Dir.exists?(lproj_folder + "/" + localizable_file)
+		if !Dir.exists?(lproj_folder)
+			FileUtils.mkdir_p lproj_folder
+			puts "Created folder: " + lproj_folder
+		end
+		# create file
+		FileUtils.cp("./" + localizable_file, lproj_folder + "/" + localizable_file)
+	end
+end
+
+
 # verify arguments
 if ARGV.length != 2
 	puts "We need exactly 2 arguments that is the path to your xcode project and location code"
@@ -16,23 +32,25 @@ location_code = ARGV[1]
 root_object = project.root_object
 project_dir = Pathname(ARGV[0]).dirname.to_s
 project_name = root_object.name
-lproj_folder = project_dir + "/" + project_name + "/" + location_code + ".lproj"
+
 
 # add region
 known_regions = root_object.known_regions
+if !known_regions.include?("en")
+	known_regions.push("en")
+end
 if !known_regions.include?(location_code)
 	known_regions.push(location_code)
 end
 
-# create .lproj folder
-if !Dir.exists?(lproj_folder)
-	FileUtils.mkdir_p lproj_folder
-	puts "Created folder: " + lproj_folder
-end
+# add locale
+add_locale(project_dir, project_name, "en")
+add_locale(project_dir, project_name, location_code)
 
 # get target
 target = project.targets.first
 
+# set flag
 target.build_configurations.each do |config|
 	config.build_settings['CLANG_ANALYZER_LOCALIZABILITY_NONLOCALIZED'] = 'YES'
 end
