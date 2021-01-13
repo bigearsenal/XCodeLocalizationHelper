249f5de2bbcd6a0c98717fb45dde62af5f98a98a
Chung Tran <bigearsenal@gmail.com>
2021-01-14 00:32:58 +0700

    let application work in background

--- macOS/LocalizationHelper/AppDelegate.swift
+++ macOS/LocalizationHelper/AppDelegate.swift
@@ -18,17 +18,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Create the SwiftUI view that provides the window contents.
         let contentView = MainView()
-
-        // Create the window and set the content view.
-//        window = NSWindow(
-//            contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
-//            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
-//            backing: .buffered, defer: false)
-//        window.isReleasedWhenClosed = false
-//        window.center()
-//        window.setFrameAutosaveName("Main Window")
-//        window.contentView = NSHostingView(rootView: contentView)
-//        window.makeKeyAndOrderFront(nil)
         
         // Set the SwiftUI's ContentView to the Popover's ContentViewController
         popover.contentSize = NSSize(width: 680, height: 300)
