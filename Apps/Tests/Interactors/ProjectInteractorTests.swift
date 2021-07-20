23998eea797ffb878b7eca80c8194e0257165196
Chung Tran <bigearsenal@gmail.com>
2021-07-20 21:19:51 +0700

    refactor InteractorTests

--- Apps/Tests/Interactors/ProjectInteractorTests.swift
+++ Apps/Tests/Interactors/ProjectInteractorTests.swift
@@ -9,27 +9,50 @@ import XCTest
 @testable import LocalizationHelper
 
 class ProjectInteractorTests: XCTestCase {
-    func testLocalizeFile() throws {
+    enum Error: Swift.Error {
+        case languageCodeNotFoundInKnownRegions
+        case fileNotFoundInLocalizableStringsGroup
+        case fileNotFound
+    }
+    
+    func testLocalizeFile1() throws {
+        try testLocalizeFile(fileName: "Test1", languageCode: "zh")
+    }
+    
+    // MARK: - Helpers
+    private func testLocalizeFile(fileName: String, languageCode: String) throws {
         let interactor = try ProjectInteractor(
-            projectRepository: FakeProjectRepository(fileName: "Test1", targetName: "Test1"),
+            projectRepository: FakeProjectRepository(fileName: fileName, targetName: fileName),
             stringsFileGenerator: StringsFileGenerator()
         )
         
         try interactor.openCurrentProject()
-        try interactor.localizeProject(languageCode: "zh")
+        try interactor.localizeProject(languageCode: languageCode)
         
+        try checkIfFileWasLocalizedCorrectly(interactor: interactor, languageCode: languageCode)
+    }
+    
+    private func checkIfFileWasLocalizedCorrectly(interactor: ProjectInteractor, languageCode: String) throws {
         // checking if knownRegions contains "zh"
         let project = interactor.appState.value.project
-        XCTAssertEqual(project?.rootObject?.knownRegions.contains("zh"), true)
+        if project?.rootObject?.knownRegions.contains(languageCode) != true {
+            throw Error.languageCodeNotFoundInKnownRegions
+        }
         
         // check if localization group contains "zh.lproj/Localizable.strings"
         let localizationGroup = project?.rootObject?.mainGroup
             .group(named: LOCALIZABLE_STRINGS, recursively: true)
-        XCTAssertEqual(localizationGroup?.children.contains(where: {$0.path == "zh.lproj/Localizable.strings"}), true)
+        
+        if localizationGroup?.children.contains(where: {$0.path == "\(languageCode).lproj/Localizable.strings"}) != true
+        {
+            throw Error.fileNotFoundInLocalizableStringsGroup
+        }
         
         // check if file exists
-        let zhFile = localizationGroup?.children.first(where: {$0.path == "zh.lproj/Localizable.strings"})
+        let zhFile = localizationGroup?.children.first(where: {$0.path == "\(languageCode).lproj/Localizable.strings"})
         let fullPath = try zhFile?.fullPath(sourceRoot: interactor.appState.value.project!.projectFolderPath)
-        XCTAssertEqual(fullPath?.exists, true)
+        if fullPath?.exists != true {
+            throw Error.fileNotFound
+        }
     }
 }
