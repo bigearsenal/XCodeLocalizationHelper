d3c2b61079b97a0d9f6db02253615335d0aee664
Chung Tran <bigearsenal@gmail.com>
2021-07-20 20:19:50 +0700

    fix all tests

---
+++ Apps/Tests/Test3/Test3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test3
+//
+//  Created by Chung Tran on 20/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
